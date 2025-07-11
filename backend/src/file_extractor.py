241566e070cdc8c66ef90ce6a5d19ae67d6648fd
AgastyaDhand1914 <agastyadhand@gmail.com>
2025-07-11 20:18:55 +0530

    switched from local to cloud llm

--- backend/src/file_extractor.py
+++ backend/src/file_extractor.py
@@ -4,9 +4,9 @@ import PyPDF2, docx
 import os
 
 class FileExtractor:
-    def __init__(self, file_storage):
-        self.file_storage = file_storage    #the file object
-        self.name = file_storage.filename   #the name of the file for checking file type
+    def __init__(self, file_path):
+        self.file_path = file_path    #the file path
+        # self.name = file_storage.filename   #the name of the file for checking file type
         self.text = ""                     #updated for extracted text from file
         self.acceptable = True             #to check if the file is acceptable for processing
 
@@ -15,9 +15,9 @@ class FileExtractor:
         #call for appropriate extraction fn as per file extension
         #only supporting .pdf and .docx for now
 
-        if self.name.endswith('.pdf'):
+        if self.file_path.endswith('.pdf'):
             return self.extract_from_pdf()
-        elif self.name.endswith('.docx'):
+        elif self.file_path.endswith('.docx'):
             return self.extract_from_docx()
         else:
             self.acceptable = False
@@ -25,26 +25,26 @@ class FileExtractor:
 
     def extract_from_pdf(self):
         try:
-            with open(self.file_storage, 'rb') as file: #read as binary file
+            with open(self.file_path, 'rb') as file: #read as binary file
                 reader = PyPDF2.PdfReader(file)
                 for page in reader.pages:   #extracted as per page
                     self.text += page.extract_text()
         except Exception as e:
             self.acceptable = False
             self.text = ""
-            raise ValueError(f"Error extracting text from {self.name}: {e}")
+            raise ValueError(f"Error extracting text from file: {e}")
         
         return self.text
 
     def extract_from_docx(self):
         try:
-            doc = docx.Document(self.file_storage)  #load docx file
+            doc = docx.Document(self.file_path)  #load docx file
             for para in doc.paragraphs:   #extracted as per paragraph
                 self.text += para.text
         except Exception as e:
             self.acceptable = False
             self.text = ""
-            raise ValueError(f"Error extracting text from {self.name}: {e}")
+            raise ValueError(f"Error extracting text from file: {e}")
         
         return self.text
 
--- backend/src/mcq_generator.py
+++ backend/src/mcq_generator.py
@@ -1,11 +1,54 @@
 #defining a class which will carry out the question generation
 
-class MCQGenerator:
-    def __init__(self, llm, prompt_template):
-        self.llm = llm
-        self.prompt_template = prompt_template
+import os
+from huggingface_hub import InferenceClient
+from langchain.prompts import PromptTemplate
+import dotenv
+dotenv.load_dotenv()
 
-    def generate_mcq(self, number, context, JSON_FORMAT):
-        prompt = self.prompt_template.format(number=number, context=context, JSON_FORMAT=JSON_FORMAT)
-        return self.llm.invoke(prompt)
+class MCQGenerator:
+    def __init__(self):
+        self.client = InferenceClient(
+                        provider="novita",
+                        api_key=os.getenv("HF_TOKEN"),
+                    )
+        self.prompt_template = PromptTemplate(
+                input_variables=["number", "context"],
+                template= '''
+                You are a knowledgeable and intellectual professor. Design {number} multiple choice
+                questions based on the following context:
+                {context}
+                The response should be a JSON object with the following format:
+
+                {{
+                    'questions' : {{
+                        "1" : {{
+                            "question" : "What is the capital of France?",
+                            "options" : {{
+                                "A" : "Paris",
+                                "B" : "London",
+                                "C" : "Berlin",
+                                "D" : "Madrid"
+                            }},
+                            "answer" : "A"
+                        }}
+                    }}
+                }}
+                '''
+            )
+
+    def generate_mcq(self, number, context):
+        formatted_prompt = self.prompt_template.format(number=number, context=context)
+        
+        completion = self.client.chat.completions.create(
+                    model="mistralai/Mistral-7B-Instruct-v0.3",
+                    messages=[
+                        {
+                            "role": "user",
+                            "content": formatted_prompt
+                        }
+                    ],
+                )
+        
+        return completion.choices[0].message.content
 
